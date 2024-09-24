2cf7060a22188cf1eff4c9e2003d2296bd874f0a
drobotmaksym <drobotmaxiim@gmail.com>
2024-09-24 12:52:38 +0200

    feat: initial commit

---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://perfect-thrush-79.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  images: {
+    domains: [
+      "files.edgestore.dev"
+    ]
+  }
+}
